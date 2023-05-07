a5759960700f9e1e8425d481449bffa19e4e43cb
mgballou <matthewbuiltthat@gmail.com>
2023-05-07 14:56:06 -0400

    Fix build error

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -19,7 +19,7 @@ function Projects(props) {
         return projects.map((project) => 
             <div>
                 <h1>{project.name}</h1>
-                <img src={project.image}/>
+                <img src={project.image} alt=""/>
                 <a href={project.git}><button>Github</button></a>
                 <a href={project.live}><button>Live Site</button></a>
             </div>
